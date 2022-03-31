a584a923a7c3137813dc098d8bdffccc60d963a6
nipengg <nevillecorneliustj@gmail.com>
2022-03-31 13:44:32 +0700

    create util function to return token

--- spotify/views.py
+++ spotify/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from werkzeug import Response
 from .credentials import REDIRECT_URI, CLIENT_ID, CLIENT_SECRET
 from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
+from .util import update_or_create_user_tokens
 
 # Create your views here.
 
@@ -39,3 +40,9 @@ def spotify_callback(request, format=None):
     expires_in = response.get('expires_in')
     error = response.get('error')
 
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
+
+    update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    return redirect('frontend:')
