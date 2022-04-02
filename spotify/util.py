c1b6196d9081ebf99dce50f7ab81bbf474535b86
nipengg <nevillecorneliustj@gmail.com>
2022-04-02 12:02:19 +0700

    get current song(still error)

--- spotify/util.py
+++ spotify/util.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
+from urllib import response
 from spotify.credentials import CLIENT_ID, CLIENT_SECRET
 from .models import SpotifyToken
 from django.utils import timezone
-from requests import post
+from requests import post, put, get
 
+BASE_URL = "https://api.spotify.com/v1/me"
 
 def get_user_tokens(session_id):
     user_tokens = SpotifyToken.objects.filter(user=session_id)
@@ -23,7 +25,7 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
         tokens.expires_in = expires_in
         tokens.token_type = token_type
         tokens.save(update_fields=['access_token',
-                    'refresh_token', 'expires_in', 'token_type'])
+                                   'refresh_token', 'expires_in', 'token_type'])
     else:
         tokens = SpotifyToken(user=session_id, access_token=access_token,
                               refresh_token=refresh_token, token_type=token_type, expires_in=expires_in)
@@ -59,3 +61,18 @@ def refresh_spotify_token(session_id):
 
     update_or_create_user_tokens(
         session_id, access_token, token_type, expires_in, refresh_token)
+
+def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_id)
+    headers = {'Content-Type': 'application/json', 'Authorization': "Bearer " + tokens.access_token}
+
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+    try:
+        return response.json()
+    except:
+        return {'Error': 'Someting went wrong...'}
--- spotify/views.py
+++ spotify/views.py
@@ -1,3 +1,4 @@
+from urllib import response
 from django.shortcuts import render, redirect
 from werkzeug import Response
 from .credentials import REDIRECT_URI, CLIENT_ID, CLIENT_SECRET
@@ -5,7 +6,8 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens, is_spotify_authenticated
+from .util import *
+from api.models import Room
 
 # Create your views here.
 
@@ -51,3 +53,18 @@ class IsAuthenticated(APIView):
     def get(self, request, format=None):
         is_authenticated = is_spotify_authenticated(self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)
+        if room.exists():
+            room = room[0]
+        else:
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+        host = room.host
+        endpoint = "player/currently-playing"
+        response = execute_spotify_api_request(host, endpoint)
+        print(response)
+
+        return Response(response, status=status.HTTP_200_OK)
